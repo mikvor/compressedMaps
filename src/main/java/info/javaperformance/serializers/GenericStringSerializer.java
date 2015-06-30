ea2a1fe47b84fd03a692e1147272a7070b01481b
Mikhail Vorontsov <mik.vorontsov@gmail.com>
2015-06-30 21:03:32 +1000

    Getting rid of any unnecessary memory allocations in GenericStringSerializer.

--- src/main/java/info/javaperformance/serializers/GenericStringSerializer.java
+++ src/main/java/info/javaperformance/serializers/GenericStringSerializer.java
@@ -21,18 +21,28 @@ package info.javaperformance.serializers;
 
 import info.javaperformance.tools.VarLen;
 
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CharsetEncoder;
+import java.nio.charset.CodingErrorAction;
 
 /**
  * String serializer accepting an encoding as an argument
+ * todo This is not a thread safe class! Thread safe version will be added once objects support will be added to MT maps.
  */
 public class GenericStringSerializer implements IObjectSerializer<String> {
-    private final Charset m_charset;
     private final int m_maxBytesPerChar;
+    private final CharsetEncoder m_encoder;
+    private final CharsetDecoder m_decoder;
+    private ByteBuffer m_ar = ByteBuffer.allocate( 64 );
+    private CharBuffer m_chars = CharBuffer.allocate( 64 );
 
     public GenericStringSerializer( final Charset charset ) {
-        m_charset = charset;
-        m_maxBytesPerChar = ( int ) Math.ceil( charset.newEncoder().maxBytesPerChar() );
+        m_encoder = charset.newEncoder().onMalformedInput( CodingErrorAction.REPLACE ).onUnmappableCharacter( CodingErrorAction.REPLACE );
+        m_decoder = charset.newDecoder().onMalformedInput( CodingErrorAction.REPLACE ).onUnmappableCharacter( CodingErrorAction.REPLACE );
+        m_maxBytesPerChar = ( int ) Math.ceil( m_encoder.maxBytesPerChar() );
     }
 
     @Override
@@ -43,9 +53,22 @@ public class GenericStringSerializer implements IObjectSerializer<String> {
             VarLen.writeSignedInt( 0, buf );
         else
         {
-            final byte[] data = v.getBytes( m_charset );
-            VarLen.writeSignedInt( data.length, buf );
-            buf.put( data, 0, data.length );
+            //extend buffers if needed
+            if ( v.length() * m_maxBytesPerChar > m_ar.capacity() )
+                m_ar = ByteBuffer.allocate( v.length() * m_maxBytesPerChar );
+            if ( v.length() > m_chars.capacity() )
+                m_chars = CharBuffer.allocate( v.length() );
+            //put a string in the input buffer
+            m_chars.clear();
+            for ( int i = 0; i < v.length(); ++i )
+                m_chars.put( v.charAt( i ) );
+            m_chars.flip();
+            m_ar.clear();
+            //and convert it
+            m_encoder.reset().encode( m_chars, m_ar, true );
+
+            VarLen.writeSignedInt( m_ar.position(), buf );
+            buf.put( m_ar.array(), 0, m_ar.position() );
         }
     }
 
@@ -59,9 +82,21 @@ public class GenericStringSerializer implements IObjectSerializer<String> {
             case 0:
                 return "";
             default:
-                final byte[] data = new byte[ len ];
-                buf.get( data, 0, data.length );
-                return new String( data, m_charset );
+                //extend buffers if needed
+                if ( m_ar.capacity() < len )
+                    m_ar = ByteBuffer.allocate( len );
+                if ( m_chars.capacity() < len ) //we can not create more than 1 char out of 1 byte
+                    m_chars = CharBuffer.allocate( len );
+                //fill the input byte buffer with the data
+                m_ar.clear();
+                m_ar.put( buf.array(), buf.position(), len );
+                m_ar.flip();
+                buf.position( buf.position() + len );
+                //now convert data
+                m_chars.clear();
+                m_decoder.decode( m_ar, m_chars, true );
+                m_chars.flip();
+                return m_chars.toString();
         }
     }
 
